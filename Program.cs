92f20eb04d270e4d4caa67b2405f23e6ca544bd1
Gerard Smit <gsmit1996@gmail.com>
2023-12-15 22:43:14 +0100

    Added sources

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading.Channels;
+using CommandLine;
+using RabbitMQ.Client;
+
+var parser = Parser.Default.ParseArguments<Options>(args);
+
+if (parser.Value is not {} options)
+{
+	return;
+}
+
+#if !FORK
+if (options.NonCopying)
+{
+	Console.WriteLine("Non-copying is only supported in the forked version");
+	return;
+}
+#endif
+
+var messageBody = RandomNumberGenerator.GetBytes(options.MessageBodySize * 1024 * 1024);
+
+var process = Process.GetCurrentProcess();
+var counter = new PerformanceCounter("Process", "Working Set - Private", process.ProcessName);
+
+var expectedCount = options.Iterations * options.Tasks;
+var elapsedTimes = new List<TimeSpan>();
+var tasks = new List<Task>();
+
+var cancellationTokenSource = new CancellationTokenSource();
+
+Console.WriteLine($"Body size	: {options.MessageBodySize} MB");
+Console.WriteLine($"Iterations	: {options.Iterations}");
+Console.WriteLine($"Tasks		: {options.Tasks}");
+Console.WriteLine($"Non-copying	: {options.NonCopying}");
+Console.WriteLine($"Startup memory	: {(int)(counter.NextValue() / 1024 / 1024)} MB");
+
+Console.WriteLine();
+Console.WriteLine("---  Start  ---");
+
+var thread = new Thread(() =>
+{
+	while (!cancellationTokenSource.IsCancellationRequested)
+	{
+		var mb = (int)(counter.NextValue() / 1024 / 1024);
+		Console.WriteLine($"Memory usage: {mb} MB");
+		Thread.Sleep(1000);
+	}
+});
+
+thread.Start();
+
+var connectionFactory = new ConnectionFactory();
+
+using (var connection = await connectionFactory.CreateConnectionAsync())
+using (var channel = await connection.CreateChannelAsync())
+{
+	channel.QueueDelete(options.QueueName);
+}
+
+for (var i = 0; i < options.Tasks; i++)
+{
+	tasks.Add(Task.Run(async () =>
+	{
+		var stopwatch = Stopwatch.StartNew();
+
+		using (var connection = await connectionFactory.CreateConnectionAsync())
+		using (var channel = await connection.CreateChannelAsync())
+		{
+			channel.QueueDeclare(options.QueueName, durable: false, exclusive: false, autoDelete: true, arguments: null);
+
+			for (var j = 0; j < options.Iterations; j++)
+			{
+				await channel.BasicPublishAsync(
+					string.Empty,
+					options.QueueName,
+					messageBody
+#if FORK
+					, copyBody: !options.NonCopying
+#endif
+				);
+			}
+		}
+
+		stopwatch.Stop();
+		elapsedTimes.Add(stopwatch.Elapsed);
+	}));
+}
+
+await Task.WhenAll(tasks);
+
+cancellationTokenSource.Cancel();
+thread.Join();
+
+Console.WriteLine();
+Console.WriteLine("--- Results ---");
+if (elapsedTimes.Count > 1)
+{
+	Console.WriteLine($"Avg time	: {(int)elapsedTimes.Average(x => x.TotalMilliseconds)} ms");
+	Console.WriteLine($"Min time	: {(int)elapsedTimes.Min(x => x.TotalMilliseconds)} ms");
+	Console.WriteLine($"Max time	: {(int)elapsedTimes.Max(x => x.TotalMilliseconds)} ms");
+}
+else
+{
+	Console.WriteLine($"Time		: {(int)elapsedTimes[0].TotalMilliseconds} ms");
+}
+
+Console.WriteLine($"Memory		: {(int)(counter.NextValue() / 1024 / 1024)} MB");
+
+using (var connection = await connectionFactory.CreateConnectionAsync())
+using (var channel = await connection.CreateChannelAsync())
+{
+	var count = channel.MessageCount(options.QueueName);
+
+	Console.WriteLine($"Queue length	: {count} / {expectedCount}");
+
+	var checkCount = Math.Min(count, 100);
+	var correctCount = 0;
+
+	Console.Write("Valid messages	: ");
+
+	for (var i = 0; i < checkCount; i++)
+	{
+		var result = await channel.BasicGetAsync(options.QueueName, true);
+
+		if (result.Body.Span.SequenceEqual(messageBody))
+		{
+			correctCount++;
+		}
+	}
+
+	Console.Write($"{correctCount} / {checkCount}");
+
+	if (checkCount == 100)
+	{
+		Console.Write($" (first 100 of {count})");
+	}
+
+	Console.WriteLine();
+
+	channel.QueueDelete(options.QueueName);
+}
+
+class Options
+{
+	[Option('i', "iterations", Required = true, HelpText = "Number of iterations")]
+	public int Iterations { get; set; }
+
+	[Option("mb", Required = true, HelpText = "Message body size in megabytes")]
+	public int MessageBodySize { get; set; }
+
+	[Option('t', "tasks", Required = true, HelpText = "Number of tasks to run")]
+	public int Tasks { get; set; }
+
+	[Option("nc", HelpText = "Don't copy the message body")]
+	public bool NonCopying { get; set; }
+
+	[Option('q', "queue", Default = "test-queue", HelpText = "Queue name")]
+	public string QueueName { get; set; } = "test-queue";
+}
---
+++ README.md
@@ -0,0 +1,110 @@
+# RabbitMQ memory test
+Note: this repository only works on Windows.
+
+It uses:
+- [Release 7.0.0-alpha.2](https://github.com/rabbitmq/rabbitmq-dotnet-client/tree/v7.0.0-alpha.2)
+- [Fork: branch `prototype/reduce-memory-usage`](https://github.com/GerardSmit/rabbitmq-dotnet-client/tree/prototype/reduce-memory-usage)
+
+## How to run
+1. Run RabbitMQ locally (e.g. using Docker) on port 5672 with the default user `guest` and password `guest`.
+2. Clone this repository (`git clone https://github.com/GerardSmit/RabbitMQ.MemoryTest.git`)
+3. Run the commands below.
+
+## Commands
+You can run the test with `dotnet run -c Release --`. You can provide the following arguments:
+
+| Short | Long | Description | Default |
+| --- | --- | --- | --- |
+| `-i` | `--iterations` | The number of iterations to run. | _required_ |
+|  | `--mb` | The size of the message in MB. | _required_ |
+| `-t` | `--tasks` | The number of tasks to run in parallel. | _required_ |
+|  | `--nc` | Disable copying the message. | `false` |
+| `-q` | `--queue` | The name of the queue to use. | `test-queue` |
+
+To use the fork, run the command with `dotnet run -c Release -p:Fork=true --`.
+
+## Examples
+Run the test with 1 MB messages, 100 iterations and 16 tasks.
+```bash
+dotnet run -c Release -- --mb=1 --iterations=100 --tasks=16
+```
+
+Run the test with 1 MB messages, 100 iterations and 16 tasks, using the fork and disabling copying the message.
+```bash
+dotnet run -c Release -p:Fork=true -- --mb=1 --iterations=100 --tasks=16 --nc
+```
+
+## Results
+The test has been run with the configuration as described in the examples above. The results are shown below.
+
+### Official client (copying)
+```
+$ dotnet run -c Release -- --mb=1 --iterations=100 --tasks=16
+Body size       : 1 MB
+Iterations      : 100
+Tasks           : 16
+Non-copying     : False
+Startup memory  : 20 MB
+
+---  Start  ---
+Memory usage: 22 MB
+Memory usage: 1947 MB
+Memory usage: 2420 MB
+Memory usage: 2700 MB
+
+--- Results ---
+Avg time        : 3408 ms
+Min time        : 3317 ms
+Max time        : 3467 ms
+Memory          : 2710 MB
+Queue length    : 1600 / 1600
+Valid messages  : 100 / 100 (first 100 of 1600)
+```
+
+### Fork (copying)
+```
+$ dotnet run -c Release -p:Fork=true -- --mb=1 --iterations=100 --tasks=16
+Body size       : 1 MB
+Iterations      : 100
+Tasks           : 16
+Non-copying     : False
+Startup memory  : 20 MB
+
+---  Start  ---
+Memory usage: 22 MB
+Memory usage: 1874 MB
+Memory usage: 2340 MB
+Memory usage: 2698 MB
+
+--- Results ---
+Avg time        : 3529 ms
+Min time        : 3450 ms
+Max time        : 3582 ms
+Memory          : 2753 MB
+Queue length    : 1600 / 1600
+Valid messages  : 100 / 100 (first 100 of 1600)
+```
+
+### Official client (non-copying)
+```
+$ dotnet run -c Release -p:Fork=true -- --mb=1 --iterations=100 --tasks=16 --nc
+Body size       : 1 MB
+Iterations      : 100
+Tasks           : 16
+Non-copying     : True
+Startup memory  : 20 MB
+
+---  Start  ---
+Memory usage: 22 MB
+Memory usage: 54 MB
+Memory usage: 59 MB
+Memory usage: 61 MB
+
+--- Results ---
+Avg time        : 3459 ms
+Min time        : 3393 ms
+Max time        : 3485 ms
+Memory          : 61 MB
+Queue length    : 1600 / 1600
+Valid messages  : 100 / 100 (first 100 of 1600)
+```
